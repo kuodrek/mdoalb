7b44c7ad6b5fc1e1e8d3b02ef9308e9de862c6d7
drek <kuodrek@gmail.com>
2022-02-04 15:07:45 -0300

    ajustado funcao tensaoAsa

--- estruturas/tensaoAsa.py
+++ estruturas/tensaoAsa.py
@@ -1,26 +1,28 @@
+import numpy as np
+
 def tensaoAsa(De, Di, T, M, Xt, k):
     # 1. Propriedades dos tubos de carbono
-    I = pi*(De**4-Di**4)/64
-    J = pi*(De**4-Di**4)/32
+    I = np.pi * (De ** 4-Di ** 4)/64
+    J = np.pi * (De ** 4-Di ** 4)/32
     
     # 2. Calculo das tensoes no ponto
-    stressP1 = [M*(De/2)*(1/I), T*(De/2)*(1/J)]
+    stressP1 = [M * (De / 2) * (1 / I), T * (De / 2) * (1 / J)]
     
     # 3. Aplicar fator de concentracao k
     stressP1 *= k; 
     
     # 4. Aplicacao do Criterio de falha de Rankine
-    sigmax = stressP1[0];
-    sigmay = 0;
-    Tau = stressP1[1];
+    sigmax = stressP1[0]
+    sigmay = 0
+    Tau = stressP1[1]
     
     # Tensoes principais
-    Sm =  (sigmax + sigmay)/2             
-    R = (((sigmax - sigmay)/2)**2+Tau**2)**0.5
+    Sm =  (sigmax + sigmay) / 2             
+    R = (((sigmax - sigmay) / 2) ** 2 + Tau ** 2) ** 0.5
     P_Stress = [abs(Sm + R), abs(Sm - R)]
     
     # Calculo do fator de seguranca
-    FS = Xt/max(P_Stress)
+    FS = Xt / max(P_Stress)
     
     return FS
 
